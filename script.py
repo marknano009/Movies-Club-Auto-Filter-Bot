1d0f4b33c7309a2a1dc95a099e298f71b74f39d6
Mark Nano <78907926+marknano009@users.noreply.github.com>
2021-03-08 08:48:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,42 +1,20 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {}
-
-You can call this as an Auto Filter Bot if you like :D
-
-Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
-
-So no need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+    START_MSG = """ <b>Hi this bot is specially made for Movies Club</b>"""
 
 
-    HELP_MSG = """<b>How to use the bot??</b>
-<i>    
-* Add user account to your channel !
+    HELP_MSG = """<b>No one Gonna help you 🤣😂🤣</b>
 
-* Add bot to group with admin rights.
-
-* Once deployed and bot is started, send a message to channel (must do step)
-
-* Bot will work now :)
-
-No need add each filter again!
-Bot will automatically search for your file and give links to that!
-</i>
-
-<b>© @TroJanzHEX</b>"""
+<b> @Malayalam_Movees</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Malayalam_Movees
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
